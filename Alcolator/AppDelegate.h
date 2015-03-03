336c16510f5ddbb8b6d0e4eb84c78e20dc154e5a
Casey Ward <caseyward1@gmail.com>
2015-03-02 17:53:47 -0800

    Initial Commit

---
+++ Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Casey Ward on 3/2/15.
+//  Copyright (c) 2015 Casey Ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Casey Ward on 3/2/15.
+//  Copyright (c) 2015 Casey Ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
